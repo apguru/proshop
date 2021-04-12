11e953479b6a8b63d68cf21f111e2fcda9f56953
Alexander Piotrowski <spielexd@hotmail.de>
2021-04-12 23:09:50 +0200

    Added Mongo DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
+import colors from "colors";
 
 dotenv.config();
 
 const app = express();
-
+connectDB();
 app.get("/", (req, res) => {
   res.send("API is running");
 });
@@ -23,5 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bold.yellow
+  )
 );
