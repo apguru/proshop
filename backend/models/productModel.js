78cb9017e585153b3ed8e557638a30f5aae94fa9
Alexander Piotrowski <spielexd@hotmail.de>
2021-04-13 14:48:46 +0200

    Added Sample data & DB Seeder

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -13,7 +13,7 @@ const reviewSchema = mongoose.Schema(
 const productSchema = mongoose.Schema(
   {
     user: {
-      type: mongoose.schema.types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
     },
