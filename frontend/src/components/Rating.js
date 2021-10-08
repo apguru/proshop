52c842fb022ab2ee334d8628bc67ca0ef0749488
Alexander Piotrowski <alexander.piotrowski@telekom.de>
2021-10-08 12:04:11 +0200

    Added Backend, Database, ErrorHandling

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Rating = ({ value, text, color }) => (
   <div className="rating">
@@ -69,12 +68,6 @@ const Rating = ({ value, text, color }) => (
 
 Rating.defaultProps = {
   color: "#f8e825",
-};
-
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
 };
 
 export default Rating;
